1cd9f502f09087a6dd70e2f7f45b6bd14c44ec29
cgendron2447 <curtis.gendron@gmail.com>
2020-02-18 03:01:29 +0000

    More geocoding fun

--- geocode.rb
+++ geocode.rb
@@ -7,3 +7,9 @@ before { puts "Parameters: #{params}" }
 get "/" do
   view "geocode"
 end
+
+get "/mapview" do
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates #=> [48.856614, 2.3522219] # latitude and longitude
+    "The coordinates of #{params["q"]} are: Latitude: #{lat_long[0]} , Longitude: #{lat_long[1]}" 
+end
